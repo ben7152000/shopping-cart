a957b01445316b58977e9813677d17712dab86fb
Ben Chou <ben7152000@gmail.com>
2021-08-11 01:30:39 +0800

    fix model

--- migrations/20210810164803-create-cart.js
+++ migrations/20210810164803-create-cart.js
@@ -8,9 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      quantity: {
-        type: Sequelize.INTEGER
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
